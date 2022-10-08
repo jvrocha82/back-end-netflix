89cd3d479744c7dd825b4ef458087ec7006be39f
jvrocha82 <jvrocha82@gmail.com>
2022-10-08 19:13:03 -0300

    define process.env.PORT with standard door

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv/config');
 var express = require('express')
 let app = express()
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 let cors = require("cors");
 app.use(cors());
 app.use('/', require('./src/routes/routes'))
 app.listen(port,() =>{
-    console.log(`listening on port http://localhost:${port}`)
+    console.log(`listening on port http://localhost:${process.env.PORT || 3000}`)
 })
